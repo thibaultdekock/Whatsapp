835f4f65745642cfd841c6ae22b06a8e69480b76
ThibaultDekock <thibault.dekock@student.kuleuven.be>
2024-11-23 15:39:33 +0100

    multithreading

--- Application/src/main/java/org/example/Main.java
+++ Application/src/main/java/org/example/Main.java
@@ -7,7 +7,22 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Run chat form
         Server.main(null);
-        Chat chatForm = new Chat();
-        Chat chatForm1 = new Chat();
+        // Start new thread for chat form
+        Thread thread = new Thread(() -> {
+            try {
+                Chat chatForm = new Chat();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            try {
+                Chat chatForm = new Chat();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        thread2.start();
+        thread.start();
     }
 }
